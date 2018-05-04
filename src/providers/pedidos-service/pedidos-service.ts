d6a50667627be3c15d4bbf9f48ad72d6061b465a
jorge <joropeza@bism.com.mx>
2018-05-04 14:51:43 -0500

    service

--- src/providers/pedidos-service/pedidos-service.ts
+++ src/providers/pedidos-service/pedidos-service.ts
@@ -4,27 +4,30 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class PedidosServiceProvider {
-
+  public url : string;
   constructor(public http: HttpClient) {
+    this.url = 'http://localhost:3000';
   }
 
   getAll(fecha){
-    return this.http.post('http://localhost:3000/pedidos',fecha)
+    return this.http.post(this.url+ '/pedidos',fecha)
   }
 
 
   getDeliv(fecha){
-    return this.http.post('http://localhost:3000/entregas',fecha)
+    return this.http.post(this.url+ '/entregas',fecha)
   }
 
   saveCot(datos){
-    return this.http.post('http://localhost:3000/cotizacion',datos)
+    return this.http.post(this.url+ '/cotizacion',datos)
   }
 
   getOne(token){
-    return this.http.get('http://localhost:3000/pedidos' + '/' +token)
+    return this.http.get(this.url+ '/pedidos/' + token)
   }
+
   changeSt(token){
-    return this.http.put('http://localhost:3000/pedidos' + '/' +token)
+    return this.http.put(this.url+ '/pedidos/' + token)
   }
+
 }
